420f8c10848b1c29b75e8f2902c0266ee7d15829
kennyzcx <kt3062@nyu.edu>
2025-09-24 19:21:37 -0400

    Update kt3062_hw1_q6.py

--- Homework/kt3062_hw1_q6.py
+++ Homework/kt3062_hw1_q6.py
@@ -1,62 +1,71 @@
 class Vector:
     def __init__(self, d):
-        if isinstance(d, int):
-            self.coords = [0] * d
-        else:
+        if type(d) == list:
             self.coords = d
-
+        else:
+            self.coords = [0]*d
+        
     def __len__(self):
         return len(self.coords)
-
+    
     def __getitem__(self, j):
         return self.coords[j]
-
+    
     def __setitem__(self, j, val):
         self.coords[j] = val
-
+        
     def __add__(self, other):
         if (len(self) != len(other)):
             raise ValueError("dimensions must agree")
         result = Vector(len(self))
         for j in range(len(self)):
-            result[j] = self[j] + other[j]
+           result[j] = self[j] + other[j]
         return result
+    
+    def __eq__(self, other):
+        return self.coords == other.coords
+    
+    def __ne__(self, other):
+        return not (self == other)
+    
+    def __str__(self):
+        return '<'+ str(self.coords)[1:-1] + '>'
+    
+    def __repr__(self):
+        return str(self)
+
+    def __neg__(self):
+        v2 = [-1*i for i in self.coords]
+        return Vector(v2)
 
     def __sub__(self, other):
         if (len(self) != len(other)):
             raise ValueError("dimensions must agree")
         result = Vector(len(self))
         for j in range(len(self)):
-            result[j] = self[j] - other[j]
+           result[j] = self[j] - other[j]
         return result
 
-    def __neg__(self):
-        result = Vector([-i for i in self])
-        return result
-
-    def __mul__(self, other):
-        if isinstance(other, int):
-            result = Vector([i * other for i in self])
-        else:
-            if (len(self) != len(other)):
+    def __mul__(self, k):
+        if type(k) == Vector:
+            if (len(self) != len(k)):
                 raise ValueError("dimensions must agree")
-            result = 0
-            for i in range(len(self)):
-                result += self[i] * other[i]
-        return result
-
-    def __rmul__(self, n):
-        result = Vector([i * n for i in self])
-        return result
-
-    def __eq__(self, other):
-        return self.coords == other.coords
-
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __str__(self):
-        return '<' + str(self.coords)[1:-1] + '>'
-
-    def __repr__(self):
-        return str(self)
+            total_mul = sum([self[i]*k[i] for i in range(len(self.coords))])
+            return total_mul
+        else:
+            product = Vector(len(self.coords))
+            for i in range(len(self.coords)):
+                product[i] = k*self.coords[i]
+            return product
+        
+    def __rmul__(self, k):
+        if type(k) == Vector:
+            if (len(self) != len(k)):
+                raise ValueError("dimensions must agree")
+            total_mul = sum([self[i]*k[i] for i in range(len(self.coords))])
+            return total_mul
+        else:
+            product = Vector(len(self.coords))
+            for i in range(len(self.coords)):
+                product[i] = k*self.coords[i]
+            return product
