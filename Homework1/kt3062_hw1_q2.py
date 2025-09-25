c8db7fafa410492217d716e4d1bcc23a9173761c
kennyzcx <kt3062@nyu.edu>
2025-09-24 23:57:08 -0400

    Update kt3062_hw1_q2.py

--- Homework1/kt3062_hw1_q2.py
+++ Homework1/kt3062_hw1_q2.py
@@ -1,13 +1,13 @@
-def shift_a(lst, k):
+def shift(lst, k):
     for i in range(k):
         lst.append(lst.pop(0))
     return lst
 
-def shift_b(lst, k , direction="left"):
+def shift(lst, k , direction="left"):
     if direction == "left":
         for i in range(k):
             lst.append(lst.pop(0))
     elif direction == "right":
         for i in range(k):
-            lst.instert(0,lst.pop(-1))
+            lst.insert(0,lst.pop(-1))
     return lst
