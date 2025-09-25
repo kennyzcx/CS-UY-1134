80dc775b36c7a1fb87000c331da3dd859d16daab
kennyzcx <kt3062@nyu.edu>
2025-09-24 21:17:09 -0400

    Update and rename kt3062_hq1_q4.py to kt3062_hq1_q4.py

--- Homework/kt3062_hq1_q4.py
+++
@@ -1,3 +0,0 @@
-[10 ** i for i in range(6)]
-[i * (i + 1) for 1 in range(10)]
-[chr(i) for i in range (97, 123)]
---
+++ Homework1/kt3062_hq1_q4.py
@@ -0,0 +1,8 @@
+#part a
+lst = [10**i for i in range(6)]
+
+#part b
+lst2 = [i*(i-1) for i in range(1,11)]
+
+#part c
+lst3 = [chr(i) for i in range(97,122)]
